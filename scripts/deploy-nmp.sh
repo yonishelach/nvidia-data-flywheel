894e18d5deb0f83982af7bf3323013771703d698
yonishelach <yonatanshelach@gmail.com>
2025-06-17 15:44:08 +0100

    change the insecure registry from dns to ip

--- scripts/deploy-nmp.sh
+++ scripts/deploy-nmp.sh
@@ -381,7 +381,7 @@ start_minikube() {
     --cpus=no-limit \
     --memory=no-limit \
     --gpus=all \
-    --insecure-registry="registry.kube-system.svc.cluster.local:5000" \
+    --insecure-registry="$(minikube ip):5000" \
     $extra_args
 
   log "Enabling ingress addon..."
