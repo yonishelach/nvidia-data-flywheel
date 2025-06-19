a441ea5eace3b5c15d79bf796ff6962640a7b932
yonishelach <yonatanshelach@gmail.com>
2025-06-19 14:37:45 +0100

    fix input

--- src/mlrun/functions/create_dataset.py
+++ src/mlrun/functions/create_dataset.py
@@ -83,17 +83,47 @@
 #     return False
 
 import mlrun
+from datetime import datetime
+from src.api.models import DataSplitConfig, FlywheelRun, TaskResult
 from src.tasks.tasks import create_datasets
+from src.lib.nemo.llm_as_judge import LLMAsJudge
 
-def create_dataset(context: mlrun.MLClientCtx, workload_id: str, client_id: str):
+def create_dataset(
+    context: mlrun.MLClientCtx,
+    workload_id: str,
+    client_id: str,
+    data_split_config: dict = None,
+):
     """
     Create datasets for a given workload and client ID.
 
-    :param context:     MLRun context.
-    :param workload_id: ID of the workload.
-    :param client_id:   ID of the client.
+    :param context:           MLRun context.
+    :param workload_id:       ID of the workload.
+    :param client_id:         ID of the client.
+    :param data_split_config: Configuration for data splitting.
+                              If None, will use the default configuration.
 
-    :return: A dictionary containing the workload ID, flywheel run ID, client ID, workload type, and created datasets.
+    :return: A JSON representation of the TaskResult containing the created datasets and other metadata.
     """
-    result = create_datasets(context, workload_id=workload_id, client_id=client_id)
+    llm_as_judge = LLMAsJudge()
+    llm_as_judge_cfg = llm_as_judge.config
+    split_config = DataSplitConfig(**data_split_config) if data_split_config else None
+    flywheel_run = FlywheelRun(
+        workload_id=workload_id,
+        client_id=client_id,
+        started_at=datetime.utcnow(),
+        num_records=0,  # Will be updated when datasets are created
+        nims=[],
+    )
+    flywheel_run_id = flywheel_run.id
+    previous_result = TaskResult(
+        workload_id=workload_id,
+        flywheel_run_id=flywheel_run_id,
+        client_id=client_id,
+        error=None,  # Reset any previous errors
+        datasets={},
+        llm_judge_config=llm_as_judge_cfg,
+        data_split_config=split_config,
+    )
+    result = create_datasets(previous_result=previous_result)
     return result.model_dump(mode="json")
