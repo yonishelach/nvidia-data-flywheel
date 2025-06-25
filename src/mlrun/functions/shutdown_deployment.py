6ddb3117300325aa7c227f2ac472f12b0dfa4cd3
yonishelach <yonatanshelach@gmail.com>
2025-06-25 11:02:46 +0300

    Fix shutdown

--- src/mlrun/functions/shutdown_deployment.py
+++ src/mlrun/functions/shutdown_deployment.py
@@ -1,17 +1,27 @@
 import mlrun
-from src.api.models import TaskResult
 from src.tasks.tasks import initialize_db_manager, shutdown_deployment as shutdown_task
 
 
-def shutdown_deployment(context: mlrun.MLClientCtx, previous_result: dict) -> dict:
+def shutdown_deployment(
+    context: mlrun.MLClientCtx,
+    base_eval_result: dict,
+    icl_eval_result: dict,
+    customization_eval_result: dict,
+) -> dict:
     """
     Shutdown the deployment for a given task result.
 
-    :param context:         MLRun context.
-    :param previous_result: Previous task result containing necessary configurations.
+    :param context:                   MLRun context.
+    :param base_eval_result:          Base evaluation results to be included in the shutdown.
+    :param icl_eval_result:           In-context learning evaluation results to be included in the shutdown.
+    :param customization_eval_result: Customization evaluation results to be included in the shutdown.
 
     :return: Updated TaskResult with shutdown status.
     """
     initialize_db_manager()
-    previous_result = TaskResult(**previous_result)
-    return shutdown_task.run(previous_result=previous_result)
+    previous_results = [
+        base_eval_result,
+        icl_eval_result,
+        customization_eval_result,
+    ]
+    return shutdown_task.run(previous_results=previous_results)
