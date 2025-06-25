1653538aa309bcd62dd5bcd0c4215b8c9713c273
yonishelach <yonatanshelach@gmail.com>
2025-06-25 15:09:20 +0300

    sequential for loop in workflow

--- src/mlrun/workflows/data_flywheel_workflow.py
+++ src/mlrun/workflows/data_flywheel_workflow.py
@@ -1,20 +1,83 @@
-# Copyright 2025 Iguazio
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import kfp
 import mlrun
 import kfp.dsl as dsl
+from kfp.dsl import component
+
+@component
+def sequential_deployment(
+    configs: list,
+    dataset: str,
+):
+    # Get the project:
+    project = mlrun.get_current_project()
+    spin_up_function = project.get_function("spin-up-nims", ignore_cache=True)
+    evaluate_function = project.get_function("evaluate", ignore_cache=True)
+    customize_function = project.get_function("customize", ignore_cache=True)
+    shutdown_function = project.get_function("shutdown-deployment", ignore_cache=True)
+
+    prev_result = dataset
+
+    for i, config in enumerate(configs):
+        spin_up_result = project.run_function(
+            spin_up_function,
+            name=f"spin-up-nim-{i}",
+            inputs={"previous_result": prev_result},
+            params={"nim_config": config},
+            returns=["previous_result: file"],
+        )
+
+        base_eval_result = project.run_function(
+            evaluate_function,
+            name=f"base-evaluate-{i}",
+            handler="run_base_eval",
+            inputs={
+                "previous_result": spin_up_result.outputs["previous_result"],
+            },
+            returns=["previous_result: file"],
+        )
+
+        icl_eval_result = project.run_function(
+            evaluate_function,
+            name=f"icl-evaluate-{i}",
+            handler="run_icl_eval",
+            inputs={
+                "previous_result": spin_up_result.outputs["previous_result"],
+            },
+            returns=["previous_result: file"],
+            local=False,
+        )
+
+        customize_result = project.run_function(
+            customize_function,
+            name=f"customize-{i}",
+            inputs={
+                "previous_result": spin_up_result.outputs["previous_result"],
+            },
+            returns=["previous_result: file"],
+        )
+
+        customization_eval_result = project.run_function(
+            evaluate_function,
+            name=f"customize-evaluate-{i}",
+            handler="run_customization_eval",
+            inputs={
+                "previous_result": customize_result.outputs["previous_result"],
+            },
+            returns=["previous_result: file"],
+        )
+
+        shutdown_result = project.run_function(
+            shutdown_function,
+            name=f"shutdown-deployment-{i}",
+            inputs={
+                "base_eval_result": base_eval_result.outputs["previous_result"],
+                "icl_eval_result": icl_eval_result.outputs["previous_result"],
+                "customization_eval_result": customization_eval_result.outputs["previous_result"],
+            },
+            returns=["previous_result: file"],
+        )
+
+        prev_result = shutdown_result.outputs["previous_result"]
 
 @dsl.pipeline()
 def pipeline(
@@ -46,76 +109,9 @@ def pipeline(
         returns=["previous_result: file"],
     )
 
-    spin_up_function = project.get_function("spin-up-nims", ignore_cache=True)
-    evaluate_function = project.get_function("evaluate", ignore_cache=True)
-    customize_function = project.get_function("customize", ignore_cache=True)
-    shutdown_function = project.get_function("shutdown-deployment", ignore_cache=True)
-
-    previous_shutdown_result = None
-    with dsl.ParallelFor(configs) as config:
-        spin_up_result = spin_up_function.as_step(
-            image=project.default_image,
-            name=f"spin-up-nim",
-            inputs={
-                "previous_result": wait_result.outputs["previous_result"],
-            },
-            params={
-                "nim_config": config,
-            },
-            returns=["previous_result: file"],
-        )
-        if previous_shutdown_result:
-            spin_up_result.after(previous_shutdown_result)
-
-        base_eval_result = project.run_function(
-            evaluate_function,
-            name=f"base-evaluate",
-            handler="run_base_eval",
-            inputs={
-                "previous_result": spin_up_result.outputs["previous_result"],
-            },
-            returns=["previous_result: file"],
-        )
-
-        icl_eval_result = project.run_function(
-            evaluate_function,
-            name=f"icl-evaluate",
-            handler="run_icl_eval",
-            inputs={
-                "previous_result": spin_up_result.outputs["previous_result"],
-            },
-            returns=["previous_result: file"],
-            local=False,
-        )
-
-        customize_result = project.run_function(
-            customize_function,
-            name=f"customize",
-            inputs={
-                "previous_result": spin_up_result.outputs["previous_result"],
-            },
-            returns=["previous_result: file"],
-        )
-
-        customization_eval_result = project.run_function(
-            evaluate_function,
-            name=f"customize-evaluate",
-            handler="run_customization_eval",
-            inputs={
-                "previous_result": customize_result.outputs["previous_result"],
-            },
-            returns=["previous_result: file"],
-        )
-
-        shutdown_result = project.run_function(
-            shutdown_function,
-            name=f"shutdown-deployment",
-            inputs={
-                "base_eval_result": base_eval_result.outputs["previous_result"],
-                "icl_eval_result": icl_eval_result.outputs["previous_result"],
-                "customization_eval_result": customization_eval_result.outputs["previous_result"],
-            },
-            returns=["previous_result: file"],
-        )
-        # Update for next iteration
-        previous_shutdown_result = shutdown_result
+    sequential_deployment(
+        configs=configs,
+        workload_id=workload_id,
+        client_id=client_id,
+        dataset=wait_result.outputs["previous_result"],
+    )
