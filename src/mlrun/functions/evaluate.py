996cc24813345ff8c0cb47f9ccbfa21b8252d377
yonishelach <yonatanshelach@gmail.com>
2025-06-29 16:55:02 +0300

    initial test for evaluation

--- src/mlrun/functions/evaluate.py
+++ src/mlrun/functions/evaluate.py
@@ -1,4 +1,7 @@
 import mlrun
+from concurrent.futures import ThreadPoolExecutor
+import time
+from src.api.job_service import get_job_details
 from src.api.models import TaskResult
 from src.tasks.tasks import (
     initialize_db_manager,
@@ -19,7 +22,21 @@ def run_base_eval(context: mlrun.MLClientCtx, previous_result: dict) -> dict:
     """
     initialize_db_manager()
     previous_result = TaskResult(**previous_result)
-    return base_eval_task.run(previous_result=previous_result)
+
+    # Wait for the thread to finish and meanwhile update the artifact from the task:
+    with ThreadPoolExecutor() as executor:
+        future = executor.submit(base_eval_task, previous_result)
+        while not future.done():
+            # Get in-progress status:
+            details = get_job_details(previous_result.flywheel_run_id)
+            print("Base evaluation in progress...")
+            print("=" * 20)
+            print(details)
+            print("=" * 20)
+            # Sleep for a while before checking again
+            time.sleep(5)
+        return future.result()  # Blocks until the thread finishes
+
 
 
 def run_icl_eval(context: mlrun.MLClientCtx, previous_result: dict) -> dict:
